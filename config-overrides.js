7e86674c12f9a05aa50cb81e23583964737d1782
lijundong <leeonfield@foxmail.com>
2019-10-09 15:24:11 +0800

    :wrench: webpack support alias

--- config-overrides.js
+++ config-overrides.js
@@ -1,9 +1,18 @@
+const path = require('path')
 const {
   override,
   useBabelRc,
   useEslintRc,
-  addDecoratorsLegacy
+  addDecoratorsLegacy,
+  addWebpackAlias,
 } = require('customize-cra')
-const config = override(addDecoratorsLegacy(), useBabelRc(), useEslintRc())
+const config = override(
+  addDecoratorsLegacy(),
+  useBabelRc(),
+  useEslintRc(),
+  addWebpackAlias({
+    ['@']: path.resolve(__dirname, 'src/'),
+  }),
+)
 
 module.exports = config
