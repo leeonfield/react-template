4b1134b56150606a026f9adc013cd19d8d407be8
lijundong <leeonfield@foxmail.com>
2019-10-22 15:57:32 +0800

    :wrench: update config-override

--- config-overrides.js
+++ config-overrides.js
@@ -5,11 +5,22 @@ const {
   useEslintRc,
   addDecoratorsLegacy,
   addWebpackAlias,
+  addLessLoader,
+  fixBabelImports,
 } = require('customize-cra')
 const config = override(
   addDecoratorsLegacy(),
   useBabelRc(),
   useEslintRc(),
+  fixBabelImports('import', {
+    libraryName: 'antd',
+    libraryDirectory: 'es',
+    style: 'css',
+  }),
+  addLessLoader({
+    javascriptEnabled: true,
+    modifyVars: { '@primary-color': '#1DA57A' },
+  }),
   addWebpackAlias({
     ['@']: path.resolve(__dirname, 'src/'),
   }),
